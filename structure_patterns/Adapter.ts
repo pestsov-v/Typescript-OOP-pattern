5151be11fd45ca7a868275051c6bfcebfffa1c59
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-28 20:39:14 +0300

    adapter pattern

---
+++ structure_patterns/Adapter.ts
@@ -0,0 +1,24 @@
+class KVDatabase {
+	private db: Map<string, string> = new Map()
+	save(key: string, value: string) {
+		this.db.set(key, value)
+	}
+}
+
+class PersistentDB {
+	savePersistent(_: Object) {}
+} 
+
+class PersistentDBAdapter extends KVDatabase {
+	constructor(public database: PersistentDB) {
+		super()
+	}
+
+	override save(key: string, value: string): void {
+		this.database.savePersistent({key, value})
+	}
+}
+
+function run (base: KVDatabase) {
+	base.save('key', 'value')
+}
