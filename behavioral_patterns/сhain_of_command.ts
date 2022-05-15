b45fae15b488fcd2236cca15798f260fcc610b40
Vladislav Pestsov <pestsov.js@gmail.com>
2022-05-15 21:30:35 +0300

    add the chain of command pattern

---
+++ behavioral_patterns/сhain_of_command.ts
@@ -0,0 +1,73 @@
+
+
+interface IMiddleware {
+    next(mid: IMiddleware): IMiddleware;
+    handle(request: any): any;
+}
+
+abstract class AbstractMiddleware implements IMiddleware{
+    private nextMiddleware: IMiddleware
+
+    next(mid: IMiddleware): IMiddleware {
+        this.nextMiddleware = mid;
+        return mid
+    }
+
+    handle(request: any): any {
+        if (this.nextMiddleware) {
+            return this.nextMiddleware.handle(request)
+        }
+
+        return
+    }
+}
+
+class AuthMiddleware extends AbstractMiddleware {
+    override handle(request: any): any {
+        console.log('AuthMiddleware');
+
+        if (request.userId === 1) {
+            return super.handle(request)
+        }
+
+        return { error: 'Вы не авторизованы' }
+    }
+}
+
+class ValidateMiddleware extends AbstractMiddleware {
+    override handle(request: any): any {
+        console.log('ValidateMiddleware');
+
+        if (request.body) {
+            return super.handle(request)
+        }
+
+        return { error: 'Нет тела запроса' }
+    }
+}
+
+class Controller extends AbstractMiddleware {
+    override handle(request: any): any {
+        console.log('Controller')
+        return { success: request }
+    }
+}
+
+const controller = new Controller()
+const validate = new ValidateMiddleware()
+const auth = new AuthMiddleware()
+
+auth.next(validate).next(controller);
+
+console.log(auth.handle({
+    userId: 3
+}))
+
+console.log(auth.handle({
+    userId: 1
+}))
+
+console.log(auth.handle({
+    userId: 1,
+    body: 'Я тело запроса'
+}))
